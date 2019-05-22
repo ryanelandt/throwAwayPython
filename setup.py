a705363bd0397814d9eb8f61dc11f2a5c89fb966
“Ryan <ryan.elandt@gmail.com>
2019-05-22 18:32:50 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='myFuns',
+      version='0.0.0',
+      url='https://github.com/ryanelandt/throwAwayPython',
+      author='Ryan Elandt',
+      author_email='ryan.elandt@gmail.com',
+      packages='myFuns',
+)
